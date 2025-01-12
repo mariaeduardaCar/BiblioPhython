4509723e3839800a693aa7ed4e53fa0f9dba4cf2
mariaeduardaCar <SeuEmailDeGitHub>
2025-01-12 00:18:12 -0300

    Atualizando configuração do banco

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 DB_CONFIG = {
-    'host': 'dpg-cu1fo75svqrc73esa5ig-a',  # Substitua pelo host do PostgreSQL no Render, ex.: 'db.internal.render.com'
-    'user': 'gerenciador_livros_user',
-    'password': '8tGzrQwgERA8jAdlIeXCuS5MzHM4MmNL',
-    'dbname': 'gerenciador_livros',  # Em PostgreSQL, o parâmetro é 'dbname', não 'db'
-    'port': 5432  # Porta padrão do PostgreSQL
+    'host': 'c9pv5s2sq0i76o.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com',
+    'user': 'uf3uml6ni9h6js',
+    'password': 'pdef26903eaaf81e4645b81426de68949f195b3114e97581349dacdcde3254cf6',
+    'dbname': 'dbgdoj5djua7n7',
+    'port': 5432  # A porta padrão do PostgreSQL
 }
+
 
 GOOGLE_BOOKS_API_URL = "https://www.googleapis.com/books/v1/volumes"
 GOOGLE_API_CHAVE = "AIzaSyBc5R8PW7Wl4JJo7KFTRGMqsBcxRx0NNag"
+
+
